cb1c1c4af0e249ecb9406719c8697ff20f76d241
saiharshith22 <saiharshithjyothula@gmail.com>
2025-02-14 23:58:14 +0530

    Update environment variables for AWS and OAuth configuration; refactor Profile component to use local video data

--- src/amplifyConfig.ts
+++ src/amplifyConfig.ts
@@ -1,20 +1,20 @@
-const awsconfig = {
-  aws_project_region: "eu-north-1", // ✅ Your AWS region
-  aws_cognito_identity_pool_id:
-    "eu-north-1:27ece60b-49b7-4e48-8595-2af5cde38f46",
-  aws_cognito_region: "eu-north-1",
-  aws_user_pools_id: "eu-north-1_1Y4Hx9gds", // ✅ Cognito User Pool ID
-  aws_user_pools_web_client_id: "2qbe4msu2fj75d5m03jvjll6m6", // ✅ Cognito App Client ID
-  oauth: {
-    domain: "eu-north-11y4hx9gds.auth.eu-north-1.amazoncognito.com",
-    scope: ["openid", "profile", "email"],
-    redirectSignIn: "http://localhost:3000/",
-    redirectSignOut: "http://localhost:3000/",
-    responseType: "code", // ✅ Use "code" for PKCE
-    googleClientId:
-      "433108609704-db81ls0o6m7ittfd5gj0cvald67uktku.apps.googleusercontent.com", // ✅ Google Client ID
-  },
-};
+// const awsconfig = {
+//   aws_project_region: "eu-north-1", // ✅ Your AWS region
+//   aws_cognito_identity_pool_id:
+//     "eu-north-1:27ece60b-49b7-4e48-8595-2af5cde38f46",
+//   aws_cognito_region: "eu-north-1",
+//   aws_user_pools_id: "eu-north-1_1Y4Hx9gds", // ✅ Cognito User Pool ID
+//   aws_user_pools_web_client_id: "2qbe4msu2fj75d5m03jvjll6m6", // ✅ Cognito App Client ID
+//   oauth: {
+//     domain: "eu-north-11y4hx9gds.auth.eu-north-1.amazoncognito.com",
+//     scope: ["openid", "profile", "email"],
+//     redirectSignIn: "http://localhost:3000/",
+//     redirectSignOut: "http://localhost:3000/",
+//     responseType: "code", // ✅ Use "code" for PKCE
+//     googleClientId:
+//       "433108609704-db81ls0o6m7ittfd5gj0cvald67uktku.apps.googleusercontent.com", // ✅ Google Client ID
+//   },
+// };
 
 // export default awsconfig;
 
@@ -36,22 +36,22 @@ const awsconfig = {
 //   },
 // };
 
-// const awsconfig = {
-//   aws_project_region: "us-east-1",
-//   aws_cognito_identity_pool_id:
-//     "us-east-1:6b8d24d4-a0c0-4f5d-9be3-d46102e8f26a",
-//   aws_cognito_region: "us-east-1",
-//   aws_user_pools_id: "us-east-1_tQrc0Zqju",
-//   aws_user_pools_web_client_id: "62nj24g9bpg0nere7g35bf5blr",
-//   oauth: {
-//     domain: "us-east-1tqrc0zqju.auth.us-east-1.amazoncognito.com",
-//     scope: ["openid", "profile", "email"],
-//     redirectSignIn: "http://localhost:3000/",
-//     redirectSignOut: "http://localhost:3000/",
-//     responseType: "code", // Use "code" for PKCE
-//     googleClientId:
-//       "308921932289-20n7laj11t16tjomhk655m6fki9um323.apps.googleusercontent.com",
-//   },
-// };
+const awsconfig = {
+  aws_project_region: import.meta.env.VITE_AWS_PROJECT_REGION,
+  aws_cognito_identity_pool_id: import.meta.env
+    .VITE_AWS_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_region: import.meta.env.VITE_AWS_COGNITO_REGION,
+  aws_user_pools_id: import.meta.env.VITE_AWS_USER_POOLS_ID,
+  aws_user_pools_web_client_id: import.meta.env
+    .VITE_AWS_USER_POOLS_WEB_CLIENT_ID,
+  oauth: {
+    domain: import.meta.env.VITE_OAUTH_DOMAIN,
+    scope: import.meta.env.VITE_OAUTH_SCOPE.split(","),
+    redirectSignIn: import.meta.env.VITE_OAUTH_REDIRECT_SIGNIN,
+    redirectSignOut: import.meta.env.VITE_OAUTH_REDIRECT_SIGNOUT,
+    responseType: import.meta.env.VITE_OAUTH_RESPONSE_TYPE,
+    googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+  },
+};
 
 export default awsconfig;
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -4,12 +4,12 @@ import styled from "styled-components";
 import IconButton from "../../components/icon-button/IconButton";
 import ProfileCard from "../../components/profile-card/ProfileCard";
 import VideoHeader from "../../components/video-card/VideoHeader";
-import { useUsernameStore } from "../../store/useUsernameStore";
+// import { useUsernameStore } from "../../store/useUsernameStore";
 
 const Profile = () => {
-  const {
-    userProfileData: { videos },
-  } = useUsernameStore();
+  // const {
+  //   userProfileData: { videos },
+  // } = useUsernameStore();
 
   // const sampleVideos = [
   //   "https://videos.pexels.com/video-files/6853337/6853337-uhd_1440_2732_25fps.mp4",
@@ -37,6 +37,289 @@ const Profile = () => {
       divRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
+
+  const videos = [
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468864000026086687",
+      image: "",
+      desc: "whitening teeth with Pam spray and Salt!",
+      stats: "",
+      statsV2: "",
+      comment: 1,
+      share: 0,
+      play: 129,
+      digg: 3,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468863987078270238",
+      image: "",
+      desc: "whitening teeth with Pam spray and Salt!",
+      stats: "",
+      statsV2: "",
+      comment: 4,
+      share: 0,
+      play: 544,
+      digg: 9,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468861981924035871",
+      image: "",
+      desc: "Look what happens when she turns it on...",
+      stats: "",
+      statsV2: "",
+      comment: 1,
+      share: 0,
+      play: 120,
+      digg: 1,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468861956099804447",
+      image: "",
+      desc: "Look what happens when she turns it on...",
+      stats: "",
+      statsV2: "",
+      comment: 0,
+      share: 0,
+      play: 32,
+      digg: 0,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468495145910979870",
+      image: "",
+      desc: "Watch what happen when they pull the last string\u2026 \ud83d\ude2f",
+      stats: "",
+      statsV2: "",
+      comment: 0,
+      share: 0,
+      play: 204,
+      digg: 1,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468494940251639071",
+      image: "",
+      desc: "Watch what happen when they pull the last string\u2026 \ud83d\ude2f",
+      stats: "",
+      statsV2: "",
+      comment: 0,
+      share: 0,
+      play: 189,
+      digg: 0,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468494601666448670",
+      image: "",
+      desc: "This pizza delivery prank made her scream ! \ud83d\ude02",
+      stats: "",
+      statsV2: "",
+      comment: 0,
+      share: 0,
+      play: 190,
+      digg: 1,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468494563984952606",
+      image: "",
+      desc: "This pizza delivery prank made her scream ! \ud83d\ude02",
+      stats: "",
+      statsV2: "",
+      comment: 3,
+      share: 1,
+      play: 882,
+      digg: 27,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468125221116054814",
+      image: "",
+      desc: "When The Cops open The Evidence Bag",
+      stats: "",
+      statsV2: "",
+      comment: 0,
+      share: 2,
+      play: 642,
+      digg: 8,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468125228023958815",
+      image: "",
+      desc: "When The Cops open The Evidence Bag",
+      stats: "",
+      statsV2: "",
+      comment: 4,
+      share: 1,
+      play: 1461,
+      digg: 8,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468124622068763934",
+      image: "",
+      desc: "The shocking impact this habit has on your health.",
+      stats: "",
+      statsV2: "",
+      comment: 0,
+      share: 0,
+      play: 475,
+      digg: 0,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7468124637260451103",
+      image: "",
+      desc: "The shocking impact this habit has on your health.",
+      stats: "",
+      statsV2: "",
+      comment: 1,
+      share: 0,
+      play: 768,
+      digg: 2,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7467745801700232478",
+      image: "",
+      desc: "THAT was in his eye\uff1f",
+      stats: "",
+      statsV2: "",
+      comment: 20,
+      share: 30,
+      play: 14649,
+      digg: 126,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7467745811506498847",
+      image: "",
+      desc: "THAT was in his eye\uff1f",
+      stats: "",
+      statsV2: "",
+      comment: 16,
+      share: 20,
+      play: 16768,
+      digg: 80,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7467744490078391582",
+      image: "",
+      desc: "That popped so loud",
+      stats: "",
+      statsV2: "",
+      comment: 241,
+      share: 3841,
+      play: 155247,
+      digg: 12310,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7467744413205237022",
+      image: "",
+      desc: "That popped so loud",
+      stats: "",
+      statsV2: "",
+      comment: 36,
+      share: 220,
+      play: 61545,
+      digg: 937,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7467034029582241054",
+      image: "",
+      desc: "That turned out amazing",
+      stats: "",
+      statsV2: "",
+      comment: 13,
+      share: 0,
+      play: 2485,
+      digg: 22,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7467034018588970270",
+      image: "",
+      desc: "That turned out amazing",
+      stats: "",
+      statsV2: "",
+      comment: 3,
+      share: 0,
+      play: 1884,
+      digg: 13,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7467029090025983263",
+      image: "",
+      desc: "SHE GOT IT RIGHT IN THE FACE \ud83e\udd23",
+      stats: "",
+      statsV2: "",
+      comment: 2,
+      share: 5,
+      play: 1365,
+      digg: 77,
+    },
+    {
+      id: "",
+      link: "",
+      video_url:
+        "https://www.tiktok.com/@riccardoo669/video/7467027998542138654",
+      image: "",
+      desc: "SHE GOT IT RIGHT IN THE FACE \ud83e\udd23",
+      stats: "",
+      statsV2: "",
+      comment: 0,
+      share: 0,
+      play: 1358,
+      digg: 37,
+    },
+  ];
 
   return (
     <div style={{ padding: "2rem" }} ref={divRef}>
